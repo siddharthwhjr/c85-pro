2b4fe7a6da008915328485ecf7f1b5484c9a7d6b
siddharthwhjr <102609874+siddharthwhjr@users.noreply.github.com>
2022-11-01 10:28:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d"); 
+
+greencar_width = 75;
+greencar_height = 100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+greencar_x = 5;
+greencar_y = 225;
+
+function add() {
+	bg_imgTag.onload = uploadBackground;
+	bg_imgTag.src = background_image;
+
+	car_imgTag.onload = uploadgreencar;
+	car_imgTag.src = greencar_image;
+}
+
+function uploadBackground() {
+	
+	ctx.drawImage(bg_imgTag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadgreencar() {
+    ctx.drawImage(car_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(greencar_y >= 0){
+		greencar_y = greencar_y - 10;
+		console.log("when you pressed up arrow x = "+greencar_x+"y = "+greencar_y);
+		uploadBackground;
+		uploadgreencar;
+}
+
+function down()
+{
+	if(greencar_y <= 500){
+		greencar_y = greencar_y + 10;
+		console.log("when you pressed down arrow x = "+greencar_x+"y = "+greencar_y);
+		uploadBackground;
+		uploadgreencar;
+	}
+}
+
+function left()
+{
+	if(greencar_x >= 0){
+		greencar_y = greencar_x - 10;
+		console.log("when you pressed left arrow x = "+greencar_x+"y = "+greencar_y);
+		uploadBackground;
+		uploadgreencar;
+	}
+}
+
+function right()
+{
+	if(greencar_x <= 800){
+		greencar_x = greencar_x + 10;
+		console.log("when you pressed right arrow x = "+greencar_x+"y = "+greencar_y);
+		uploadBackground;
+		uploadgreencar;
+	}
+}
